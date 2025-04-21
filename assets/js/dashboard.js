f7c9e695de929a865e8610e0adee166204617551
TMC1995 <cryssan28_9@hotmail.com>
2025-04-20 23:54:20 -0500

    Fun. Dashboard - Actualizaciones
    Nuevo:
    * Clase Usuario en js
    Actualización:
    * Ampliación y modificación de datos de usuario en html
    * Estructurar los datos del usuario en la clase en js
    Corrección:
    * Insertar script firebase-firestore en index y login
    * En tm638231 eliminar | en desarrollo y educación

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -4,13 +4,21 @@ auth.onAuthStateChanged(user => {
     db.collection("usuarios").doc(user.uid).get()
       .then((doc) => {
         if (doc.exists) {
-          const data = doc.data();
-          console.log("Bienvenido", data.nombre);
-          document.getElementById("usuarioNombresHeader").innerText = data.Nombres;
-          document.getElementById("usuarioNombres").innerText = data.Nombres;
-          document.getElementById("usuarioApellidos").innerText = data.Apellidos;
-          document.getElementById("usuarioCorreoLocal").innerText = data.CorreoLocal;
-          document.getElementById("usuarioUID").innerText = user.uid;
+          const usuario = new Usuario(doc);
+          console.log("Bienvenido", usuario.NombreApellido());
+          document.getElementById("usuario.NombreApellido").innerText = usuario.NombreApellido();
+          document.getElementById("usuario.Nombres").innerText = usuario.Nombres;
+          document.getElementById("usuario.Apellidos").innerText = usuario.Apellidos;
+          document.getElementById("usuario.FechaNacimiento").innerText = usuario.FechaNacimiento;
+          document.getElementById("usuario.Edad").innerText = usuario.Edad() + " años";
+          document.getElementById("usuario.Celular").innerText = usuario.Celular;
+          document.getElementById("usuario.CorreoLocal").innerText = usuario.CorreoLocal;
+          document.getElementById("usuario.CorreoPersonal").innerText = usuario.CorreoPersonal;
+          document.getElementById("usuario.CorreoUC").innerText = usuario.CorreoUC;
+          document.getElementById("usuario.Direccion").innerText = usuario.Direccion;
+          document.getElementById("usuario.Residencia").innerText = usuario.Residencia();
+          document.getElementById("usuario.CodigoPostal").innerText = usuario.CodigoPostal;
+          document.getElementById("usuario.UID").innerText = usuario.UID;
         } else {
           console.log("No se encontró el documento del usuario.");
         }
@@ -27,4 +35,64 @@ function logout() {
   auth.signOut().then(() => {
     window.location.href = "index.html"; // Redirigir al cerrar sesión
   });
+}
+
+class Usuario {
+  constructor(doc) {
+    this.UID = doc.id;
+    this.Nombres = doc.data().Nombres || "-";
+    this.Apellidos = doc.data().Apellidos || "-";
+    this.FechaNacimiento = doc.data().FechaNacimiento || "-";
+    this.Celular = doc.data().Celular || "-";
+    this.CorreoLocal = doc.data().CorreoLocal || "-";
+    this.CorreoPersonal = doc.data().CorreoPersonal || "-";
+    this.CorreoUC = doc.data().CorreoUC || "-";
+    this.Direccion = doc.data().Direccion || "-";
+    this.LugarResidencia = doc.data().LugarResidencia || {
+      Distrito: "-",
+      Provincia: "-",
+      Departamento: "-",
+      Pais: "-"
+    };
+    this.CodigoPostal = doc.data().CodigoPostal || "-";
+  }
+  NombreCompleto() {
+    if (this.Nombres === "-" || this.Apellidos === "-") {
+      return "-";
+    }
+    return `${this.Nombres} ${this.Apellidos}`;
+  }
+  NombreApellido() {
+    if (this.Nombres === "-" || this.Apellidos === "-") {
+      return "-";
+    }
+    const primerNombre = this.Nombres.split(' ')[0];
+    const primerApellido = this.Apellidos.split(' ')[0];
+    return `${primerNombre} ${primerApellido}`;
+  }
+  Edad() {
+    if (this.FechaNacimiento === "-") {
+      return "-";
+    }
+    const hoy = new Date();
+    const nacimiento = new Date(this.FechaNacimiento);
+    if (isNaN(nacimiento.getTime())) {
+      return "-";
+    }
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+    const mes = hoy.getMonth() - nacimiento.getMonth();
+    if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
+      edad--;
+    }
+    return edad;
+  }
+  Residencia() {
+    if (this.LugarResidencia.Distrito === "-" || 
+        this.LugarResidencia.Provincia === "-" || 
+        this.LugarResidencia.Departamento === "-" || 
+        this.LugarResidencia.Pais === "-") {
+      return "-";
+    }
+    return `${this.LugarResidencia.Distrito} - ${this.LugarResidencia.Provincia} - ${this.LugarResidencia.Departamento} - ${this.LugarResidencia.Pais}`;
+  }
 }
